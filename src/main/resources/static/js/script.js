40fdcf22c35040a0358e42982f66dad6f1e55243
Power_tile <hejizheng2004@163.com>
2020-05-29 20:15:52 +0800

    change dependency to view-design, finish initial dev-page with stepbar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,19 +1,12 @@
 let app = new Vue({
     el: "#app",
     data: {
-        activeStep: 0,
+        step: 0
     },
     computed: {
-        finished: function() {
-            return this.activeStep > 2;
-        }
+        
     },
     methods: {
-        handleNext: function() {
-            this.activeStep++;
-        },
-        handlePrev: function() {
-            this.activeStep--;
-        }
+        
     }
 });
