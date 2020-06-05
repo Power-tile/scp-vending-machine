ac27e7d13890d76b058882a6d0da3fe36eb9b9d2
Power_tile <hejizheng2004@163.com>
2020-06-05 23:53:12 +0800

    add fixed to header and footer

--- src/main/resources/static/js/script-dev.js
+++ src/main/resources/static/js/script-dev.js
@@ -26,6 +26,7 @@ let app = new Vue({
         selectCount: [],
         addWarning: [],
         minusWarning: [],
+        statusBarHeight: 0,
     },
     mounted() {
         let self = this;
@@ -36,6 +37,7 @@ let app = new Vue({
             self.addWarning.push(false);
             self.minusWarning.push(false);
         }
+        self.statusBarHeight = this.$refs.statusBar.offsetHeight;
     },
     computed: {
         viewProducts: function() {
