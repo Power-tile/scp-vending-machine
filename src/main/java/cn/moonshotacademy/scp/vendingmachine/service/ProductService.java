678efbd1828fa5a107e6fe87c7cdf9212aff7f39
huangsx0925 <18011027@stu.moonshotacademy.cn>
2020-06-08 18:54:40 +0800

    add product availability check commit

--- src/main/java/cn/moonshotacademy/scp/vendingmachine/service/ProductService.java
+++ src/main/java/cn/moonshotacademy/scp/vendingmachine/service/ProductService.java
@@ -1,5 +1,7 @@
 package cn.moonshotacademy.scp.vendingmachine.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,17 @@ public class ProductService {
     ProductDAO productDAO;
 
     public ProductVO getProducts(Integer test) {
-        List<ProductDTO> productDTOList = this.productDAO.findByTestAndRandom(test, 0);
         ProductVO productVO = new ProductVO();
+        List<ProductDTO> productDTOList = this.productDAO.findByTestAndRandom(test, 0);
+        int cnt = 0;
+        while(cnt<productDTOList.size()){
+            if(!this.available(productDTOList.get(cnt))){
+                productDTOList.remove(cnt);
+            } else {
+                cnt++;
+            }
+        }
+
         productVO.setProducts(productDTOList);
         productVO.setRandomBox(this.getRandomVO(test));
         return productVO;
@@ -28,15 +39,36 @@ public class ProductService {
     private RandomVO getRandomVO(Integer test) {
         RandomVO ret = new RandomVO();
         List<ProductDTO> randomDTOList = this.productDAO.findByTestAndRandom(test, 1);
-        Integer[] ids = new Integer[randomDTOList.size()];
+        ArrayList<Integer> ids = new ArrayList<Integer>();
         Double totalPrice = 0.0;
         for (int i = 0; i < randomDTOList.size(); i++) {
-            ids[i] = randomDTOList.get(i).getId();
-            totalPrice += randomDTOList.get(i).getPrice();
+            ProductDTO randomDTO = randomDTOList.get(i);
+            if (this.available(randomDTO)) {
+                ids.add(randomDTOList.get(i).getId());
+                totalPrice += randomDTOList.get(i).getPrice();
+            }
         }
-        ret.setIds(ids);
+
+        // ret.setIds((Integer[]) ids.toArray());
+        Integer[] retIds = new Integer[ids.size()];
+        for(int i=0;i<ids.size();i++){
+            retIds[i]=ids.get(i);
+        }
+        ret.setIds(retIds);
         ret.setPrice(totalPrice / randomDTOList.size());
         ret.setTest(test);
         return ret;
+    }
+
+    private boolean available(ProductDTO productDTO) {
+        Date productDate = productDTO.getExpiryDate();
+        Date currentDate = new Date();
+        // System.out.println(productDTO.getName());
+        // System.out.println(productDate);
+        if (productDate.compareTo(currentDate) == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
