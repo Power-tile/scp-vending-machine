a2b280fdcb5fc615ddf859a365df533964048403
Power_tile <hejizheng2004@163.com>
2020-06-12 22:57:28 +0800

    finish debugging session api

--- src/main/java/cn/moonshotacademy/scp/vendingmachine/listener/SessionListener.java
+++ src/main/java/cn/moonshotacademy/scp/vendingmachine/listener/SessionListener.java
@@ -13,33 +13,18 @@ import javax.servlet.http.HttpSessionListener;
 public class SessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent event) {
-        System.out.println("Session Created ---- ");
+        System.out.println("--- Session Created ---- ");
 
-        // HttpSession session = event.getSession();
-        // session.setMaxInactiveInterval(10);
-        // System.out.println("Current Session: " + session.getId());
-
-        // ServletContext application = session.getServletContext();
-        // HashSet<HttpSession> sessions = (HashSet<HttpSession>) application.getAttribute("sessions");
-        // if (sessions == null) {
-        //     sessions = new HashSet<HttpSession>();
-        //     application.setAttribute("sessions", sessions);
-        // }
-        // sessions.add(session);
-        // System.out.println("Current available sessions: " + Integer.valueOf(sessions.size()).toString());
+        HttpSession session = event.getSession();
+        session.setMaxInactiveInterval(10);
+        System.out.println("createdSessionId: " + session.getId());
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent event) throws ClassCastException {
-        System.out.println("Session Destroyed ---");
+        System.out.println("--- Session Destroyed ---");
 
-        // HttpSession session = event.getSession();
-        // System.out.println("deletedSessionId: " + session.getId());
-        // System.out.println(session.getCreationTime());
-        // System.out.println(session.getLastAccessedTime());
-
-        // ServletContext application = session.getServletContext();
-        // HashSet<HttpSession> sessions = (HashSet<HttpSession>) application.getAttribute("sessions");
-        // sessions.remove(session);
+        HttpSession session = event.getSession();
+        System.out.println("deletedSessionId: " + session.getId());
     }
 }
