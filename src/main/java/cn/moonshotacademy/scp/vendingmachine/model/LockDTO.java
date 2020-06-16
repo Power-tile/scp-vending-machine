32d5df3791987a1462baf60ba26bb796fe0fe7e0
Power_tile <hejizheng2004@163.com>
2020-06-16 10:56:04 +0800

    add LockDTO LockDAO, delete sessionId from ProductDTO

--- src/main/java/cn/moonshotacademy/scp/vendingmachine/model/LockDTO.java
+++ src/main/java/cn/moonshotacademy/scp/vendingmachine/model/LockDTO.java
@@ -1,7 +1,5 @@
 package cn.moonshotacademy.scp.vendingmachine.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,36 +9,24 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "products")
-public class ProductDTO {
+@Table(name = "locks")
+public class LockDTO {
     @Id
     @Column(name = "id")
     private Integer id;
-
+    
     @Column(name = "name")
     private String name;
 
     @Column(name = "price")
     private Double price;
 
-    @Column(name = "production_date")
-    private Date productionDate;
-
-    @Column(name = "expiry_date")
-    private Date expiryDate;
-
-    @Column(name = "status")
-    private Integer status;
-
-    @Column(name = "random")
-    private Integer random;
-
-    @Column(name = "slot")
-    private Integer slot;
-
-    @Column(name = "test")
-    private Integer test;
-
     @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "count")
+    private Integer count;
+
+    @Column(name = "session_id")
+    private String sessionId;
 }
